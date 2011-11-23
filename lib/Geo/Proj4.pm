6e601d3aec48fbd9fbba95749e479a308150ab0a
Mark Overmeer <mark@overmeer.net>
2011-11-23 09:24:35 +0100

    distribution Geo-Proj4-1.02.tar.gz

--- lib/Geo/Proj4.pm
+++ lib/Geo/Proj4.pm
@@ -289,7 +289,7 @@ sub transform($$)
 {   my ($self, $to, $points) = @_;
 
     ref $points eq 'ARRAY'
-       or croak "ERROR: transform() expects array of points";
+        or croak "ERROR: transform() expects array of points";
 
     my ($err, $errtxt, $pr);
     if(ref($points->[0]) eq 'ARRAY')
